6d7f12af400a14a2415893dabfed8746dda46a9f
Kok Hoor <kokhoor@gmail.com>
2021-10-12 11:46:11 +0800

    Restructured Action Permission

--- simplyscript/models.py
+++ simplyscript/models.py
@@ -1,47 +1,43 @@
 from django.db import models
 
 
-class Module(models.Model):
-    id = models.IntegerField(primary_key=True)
-    name = models.CharField(max_length=64, blank=False, null=False)
-    description = models.TextField()
+class ActionPermission(models.Model):
+    name = models.CharField(max_length=128, blank=False, null=False, unique=True)
+    active_ind = models.BooleanField(blank=False, null=True, default=True)
 
     def __str__(self):
         return self.name
 
     class Meta:
-        verbose_name = 'Module'
-        verbose_name_plural = 'Modules'
-
-
-class ActionPermission(models.Model):
-    module = models.ForeignKey(Module, blank=False, null=False, on_delete=models.CASCADE)
-    method = models.CharField(max_length=128)
-
-    def __str__(self):
-        return f"{self.module.name}.{self.method}"
-
-    class Meta:
         verbose_name = 'Action Permission'
         verbose_name_plural = 'Action Permissions'
-        constraints = [
-            models.UniqueConstraint(fields=['module', 'method'], name='actionpermission_module_method')
-        ]
 
 
 class GroupActionPermission(models.Model):
     group = models.ForeignKey("auth.Group", null=True, blank=False, on_delete=models.CASCADE)
     permission = models.ForeignKey(ActionPermission, blank=False, null=False, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f"{self.group.name}.{self.permission.name}"
+
     class Meta:
         verbose_name = 'Group Action Permission'
         verbose_name_plural = 'Group Action Permissions'
+        constraints = [
+            models.UniqueConstraint(fields=['group', 'permission'], name='groupactionpermission_permission')
+        ]
 
 
 class UserActionPermission(models.Model):
     user = models.ForeignKey("auth.User", null=True, blank=False, on_delete=models.CASCADE)
     permission = models.ForeignKey(ActionPermission, blank=False, null=False, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f"{self.user.username}.{self.permission.name}"
+
     class Meta:
         verbose_name = 'User Action Permission'
         verbose_name_plural = 'User Action Permissions'
+        constraints = [
+            models.UniqueConstraint(fields=['user', 'permission'], name='useractionpermission_permission')
+        ]
